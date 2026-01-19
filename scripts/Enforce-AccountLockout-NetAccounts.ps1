b1ee69ccf0003e4096f6ab7e77fc798b491f76f5
Ngozi <ashleytechpro2011@gmail.com>
2026-01-19 01:28:56 -0700

    Update Enforce-AccountLockout-NetAccounts.ps1

--- scripts/Enforce-AccountLockout-NetAccounts.ps1
+++ scripts/Enforce-AccountLockout-NetAccounts.ps1
@@ -9,19 +9,19 @@
       - Reset lockout counter: 15 minutes
 
 .NOTES
-    Author          : Malaya M.
+    Author          : Ashley U.
     LinkedIn        : https://linkedin.com/in/ashleytechpro2011
     GitHub          : https://github.com/ashleytechpro2011
-    Date Created    : 2025-07-22
-    Last Modified   : 2025-07-22
+    Date Created    : 2025-07-16
+    Last Modified   : 2025-07-16
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
     STIG-ID         : WN10-AC-000005, WN10-AC-000010, WN10-AC-000015
 
 .TESTED ON
-    Date(s) Tested  : 2025-07-22
-    Tested By       : Malaya M.
+    Date(s) Tested  : 2025-07-16
+    Tested By       : Ashley U.
     Systems Tested  : Windows 10 (10.0.19045.5487)
     PowerShell Ver. : 5.1
 
