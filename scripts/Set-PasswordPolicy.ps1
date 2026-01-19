4137d13c77a22cde0379893f3b94bb0e5fdc9323
Ngozi <ashleytechpro2011@gmail.com>
2026-01-19 01:33:09 -0700

    Update Set-PasswordPolicy.ps1

--- scripts/Set-PasswordPolicy.ps1
+++ scripts/Set-PasswordPolicy.ps1
@@ -10,19 +10,19 @@
     that are at least 14 characters long and include a mix of character types.
 
 .NOTES
-    Author          : Malaya M.
-    LinkedIn        : https://linkedin.com/in/malaya-m
-    GitHub          : https://github.com/malaya-m
-    Date Created    : 2025-07-22
-    Last Modified   : 2025-07-22
+    Author          : Ashley U.
+    LinkedIn        : https://linkedin.com/in/ashleytechpro2011
+    GitHub          : https://github.com/ashleytechpro2011
+    Date Created    : 2025-07-16
+    Last Modified   : 2025-07-16
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
     STIG-ID         : WN10-AC-000035, WN10-AC-000040
 
 .TESTED ON
-    Date(s) Tested  : 2025-07-22
-    Tested By       : Malaya M.
+    Date(s) Tested  : 2025-07-16
+    Tested By       : Ashley U. 
     Systems Tested  : Windows 10 (10.0.19045.5487)
     PowerShell Ver. : 5.1
 
